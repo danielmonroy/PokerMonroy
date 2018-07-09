95144623e6c05306178c89794849811fa1e422ae
Daniel Monroy <ldanielmonroy@gmail.com>
2018-07-08 23:02:07 -0500

    Updated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :resume
   resources :poker_games
   resources :homes
   resources :poker_players
-  devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin
+  # For details on he DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'resume#index'
 end
